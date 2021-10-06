60a29062e5f4b8aa066b541e7d2a1ca220ed163a
Akash <akashkothawade77@gmail.com>
2021-10-06 19:08:40 +0530

    arraysum

---
+++ simpleArraySum.js
@@ -0,0 +1,12 @@
+/** @format */
+
+function simpleArraySum(ar) {
+	let sum = 0;
+	for (let i = 0; i < ar.length; i++) {
+		sum += ar[i];
+	}
+	console.log(sum);
+}
+
+// const array = [1,2,3,4]
+simpleArraySum([1, 2, 3, 4]);
