f59ee93325f11d095905d910593395650b3fa0ad
Akash <akashkothawade77@gmail.com>
2021-11-03 23:27:07 +0530

    new commit

---
+++ missingNo.js
@@ -0,0 +1,13 @@
+/** @format */
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 10];
+const findMissingNum = (arr) => {
+	for (var i = 0; i < arr.length - 1; i++) {
+		if (arr[i] + 1 != arr[i + 1]) {
+			return arr[i] + 1;
+		}
+	}
+
+	return false;
+};
+console.log(findMissingNum(arr)); // Returns 9, the missing number
