f693ebf4e801634c8d818467b0bc4a5d4f2ef792
Akash <akashkothawade77@gmail.com>
2021-11-05 22:50:15 +0530

    max charhachter

---
+++ maxcharachter.js
@@ -0,0 +1,8 @@
+/** @format */
+
+//Given a string of characters, return the character that appears the most often.
+describe('Max Character', () => {
+	it('Should return max character', () => {
+		assert.equal(max('Hello World!'), 'l');
+	});
+});
