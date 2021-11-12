744c0b2d0251acc7ded2bb936cf7726e3255e9b3
Akash <akashkothawade77@gmail.com>
2021-11-12 23:05:27 +0530

    new commit

---
+++ matrixspiral.js
@@ -0,0 +1,11 @@
+/** @format */
+
+describe('Matrix Spiral', () => {
+	it('Should implement matrix spiral', () => {
+		assert.deepEqual(spiral(3), [
+			[1, 2, 3],
+			[8, 9, 4],
+			[7, 6, 5],
+		]);
+	});
+});
